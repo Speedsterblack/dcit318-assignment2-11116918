a3fd0d76d3e1f75a45d6b40cdcc0ff62d7102579
Mensah Gad Kwame <speedsterblack92@gmail.com>
2025-08-09 08:59:59 +0000

    Abstract methods

---
+++ Abstract_class_and_method/Abstract_class_and_method/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Abstract_class_and_method
+{
+    //Abstract class
+    public abstract class Shape
+    {
+        public abstract double getArea(); // Abstract method
+    }
+
+    // Derived class
+    public class Circle : Shape
+    {
+        private double radius;
+        public Circle(double r)
+        {
+            radius = r;
+        }
+        // Implementing the abstract method
+        public override double getArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+    public class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        // Implementing the abstract method
+        public override double getArea()
+        {
+            return length * width;
+        }
+    }
+    // Main program
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Shape circle = new Circle(5);
+            Console.WriteLine($"Area of Circle: {circle.getArea()}");
+            Shape rectangle = new Rectangle(4, 5);
+            Console.WriteLine($"Area of Rectangle: {rectangle.getArea()}");
+        }
+    }
+}
