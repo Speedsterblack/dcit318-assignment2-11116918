9d7bc1eabc2aee6501acab602f8c0e6a5efa4985
Mensah Gad Kwame <speedsterblack92@gmail.com>
2025-08-09 08:44:15 +0000

    Inheritance_And_Method_Overriding

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Inheritance
+{
+    // Base class
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        // Constructor
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public virtual void MakeSound()
+        {
+            Console.WriteLine($"{Name} makes a sound.");
+        }
+
+        //Derived class
+        public class Dog : Animal
+        {
+            public Dog(string name) : base(name)
+            {
+            }
+            public override void MakeSound()
+            {
+                Console.WriteLine($"{Name} barks.");
+            }
+        }
+        public class Cat : Animal
+        {
+            public Cat(string name) : base(name)
+            {
+            }
+            public override void MakeSound()
+            {
+                Console.WriteLine($"{Name} meows.");
+            }
+        }
+    }
+
+    // Main method
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myDog = new Animal.Dog("Truce");
+            Animal myCat = new Animal.Cat("Lucky");
+            myDog.MakeSound(); // Output: Truce barks.
+            myCat.MakeSound(); // Output: Lucky meows.
+        }
+    }
+}
