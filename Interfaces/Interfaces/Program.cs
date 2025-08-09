c02d1d477125e270c34f0130693c6d5f8703ab58
Mensah Gad Kwame <speedsterblack92@gmail.com>
2025-08-09 09:04:49 +0000

    Interfaces

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Interfaces
+{ 
+public interface Imovable
+    {
+        void move();
+    }
+    //derived interface
+    public class Car : Imovable
+    {
+        public void move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : Imovable
+    {
+        public void move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Imovable myCar = new Car();
+            myCar.move();
+            Imovable myBicycle = new Bicycle();
+            myBicycle.move();
+        }
+    }
+
+}
